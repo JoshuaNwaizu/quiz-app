6d7404e6bd1fa756a245c611f502c86647863862
JoshuaChris01 <joshuachris000@gmail.com>
2024-01-12 08:58:17 +0100

    New changes

--- html-quiz.js
+++ html-quiz.js
@@ -1,85 +1,86 @@
 const questions = document.getElementById('html-question')
 const choices = Array.from(document.querySelectorAll('.html__choice-text')),
-    choicePrefix = document.querySelector('.html__choice-prefix')
+    choicePrefix = document.querySelector('.html__choice-prefix'),
+    optionList = document.querySelector('.html__options-list')
 
 
 let currentQuestion = {}
-let takingAnswers = true
+let takingAnswers = false
 let score = 0
 let questionCounter = 0
 let availableQuestions = []
 
 const htmlQuestions = [
+
     {
         question: "What does HTML stand for?",
         options: {
-            A: "Hyper Text Markup Language",
-            B: "High-Level Text Management Language",
-            C: "Hyper Transfer Text Language",
-            D: "Hyperlink and Text Management Language",
+            option1: "Hyper Text Markup Language",
+            option2: "High-Level Text Management Language",
+            option3: "Hyper Transfer Text Language",
+            option4: "Hyperlink and Text Management Language",
         },
-        answer: "A"
+        answer: "1"
     },
     {
         question: "Which tag is used to define an unordered list in HTML?",
         options: {
-            A: "<ul>",
-            B: "<ol>",
-            C: "<li>",
-            D: "<ul></ul>",
+            option1: "<ul>",
+            option2: "<ol>",
+            option3: "<li>",
+            option4: "<ul></ul>",
         },
-
-        answer: "A"
+        answer: "1"
     },
     {
         question: "What is the purpose of the HTML <head> tag?",
         options: {
-            A: "It defines the main content of the document.",
-            B: "It contains metadata about the document.",
-            C: "It defines a header for the document.",
-            D: "It represents a paragraph in HTML.",
+            option1: "It defines the main content of the document.",
+            option2: "It contains metadata about the document.",
+            option3: "It defines a header for the document.",
+            option4: "It represents a paragraph in HTML.",
         },
-        answer: "B"
+        answer: "2"
     },
     {
         question: "Which attribute is used to provide additional information about an HTML element?",
         options: {
-            A: "class",
-            B: "style",
-            C: "id",
-            D: "title",
+            option1: "class",
+            option2: "style",
+            option3: "id",
+            option4: "title",
         },
-        answer: "D"
+        answer: "4"
     },
     {
         question: "In HTML, what does the acronym 'URL' stand for?",
         options: {
-            A: "Uniform Resource Locator",
-            B: "Universal Reference Language",
-            C: "Unified Resource Link",
-            D: "User Reference Locator",
+            option1: "Uniform Resource Locator",
+            option2: "Universal Reference Language",
+            option3: "Unified Resource Link",
+            option4: "User Reference Locator",
         },
-        answer: "A"
+        answer: "1"
     },
     {
         question: "Which HTML tag is used to create a hyperlink?",
         options: {
-            A: "<a>",
-            B: "<link>",
-            C: "<hyperlink>",
-            D: "<url>",
+            option1: "<a>",
+            option2: "<link>",
+            option3: "<hyperlink>",
+            option4: "<url>",
         },
-        answer: "A"
+        answer: "1"
     },
     {
         question: "What is the purpose of the HTML <footer> tag?",
         options: {
-            A: "It defines a section in the document.",
-            B: "It represents the main content of the document.",
-            C: "It contains metadata about the document.",
-            D: "It defines a footer for the document.",
+            option1: "It defines a section in the document.",
+            option2: "It represents the main content of the document.",
+            option3: "It contains metadata about the document.",
+            option4: "It defines a footer for the document.",
         },
-        answer: "D"
+        answer: "4"
     }
 ];
 
@@ -96,22 +97,35 @@ const startGame = () => {
 console.log(availableQuestions)
 
 const getNewQuestions = () => {
+    if (availableQuestions.length === 0 || questionCounter > MAX_QUESTIONS) {
+      return window.location.assign('./end.html')
+    }
+
     questionCounter++
     const questionIndex = Math.floor(Math.random() * availableQuestions.length);
     currentQuestion = availableQuestions[questionIndex]
     questions.innerText = currentQuestion.question
 
-    choices.forEach(choice => {
-        for (const quizQuetions of htmlQuestions) {
-            for (const [optionKey, optionValue] of Object.entries(quizQuetions.options)) {
-                choicePrefix.innerText = optionKey
-                choice.innerText = optionValue
-
-
-
-            }
-        }
-    })
+    choices.forEach((choice, index) => {
+        const optionKey = 'option' + (index + 1);
+        choice.innerText = currentQuestion.options[optionKey];
+        console.log(choice.innerText)
+    });
+    availableQuestions.splice(questionIndex, 1)
+    takingAnswers = true
 }
+
+choices.forEach((choice, index) => {
+    choice.addEventListener('click', e => {
+        if (!takingAnswers) return
+
+        takingAnswers = false
+        const selectedChoice = e.target
+        const selectedAnswer = index + 1
+        console.log(selectedAnswer)
+        getNewQuestions()
+    })
+})
+
 
 startGame()
