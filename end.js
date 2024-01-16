a4f1999cf5cb5885aa0a8fd16193c58216e3e126
JoshuaChris01 <joshuachris000@gmail.com>
2024-01-16 09:17:42 +0100

    New changes

--- end.js
+++ end.js
@@ -1,10 +1,19 @@
 const finalScore = document.querySelector('.score'),
-scoreRemains = document.querySelector('.score__remains')
+scoreRemains = document.querySelector('.score__remains'),
+button = document.querySelector('.end-btn')
 
 
-const mostRecentScore = localStorage.getItem('mostRecentScore')
-const MAX_QUESTIONS = localStorage.getItem('MAX_QUESTIONS')
-const questionCounter = localStorage.getItem('questionCounter')
+const mostRecentScore = localStorage.getItem('mostRecentScore'),
+ MAX_QUESTIONS = localStorage.getItem('MAX_QUESTIONS'),
+questionCounter = localStorage.getItem('questionCounter'),
+finalScoreText = localStorage.getItem('finalScore')
 
-scoreRemains.innerText = `out of ${MAX_QUESTIONS}`
-finalScore.innerText = questionCounter
+const theFinalText = parseInt(finalScoreText) + 1
+scoreRemains.innerText = `Got ${theFinalText} out of ${MAX_QUESTIONS} questions`
+finalScore.innerText = mostRecentScore
+
+
+function handleClick() {
+    return window.location.assign('index.html')
+}
+button.addEventListener('click', handleClick)
--- html-quiz.js
+++ html-quiz.js
@@ -13,6 +13,7 @@ let takingAnswers = false
 let score = 0
 let questionCounter = 0
 let availableQuestions = []
+let finalScore = 0
 
 const htmlQuestions = [
 
@@ -94,6 +95,7 @@ const MAX_QUESTIONS = 7
 const startGame = () => {
     questionCounter = 0
     score = 0
+  
     availableQuestions = [...htmlQuestions]
     getNewQuestions()
 }
@@ -102,17 +104,19 @@ const getNewQuestions = () => {
     if (availableQuestions.length === 0 || questionCounter >= MAX_QUESTIONS) {
         return window.location.assign('end.html')
     }
-        localStorage.setItem('mostRecentScore', score)
-        localStorage.setItem('questionCounter', questionCounter)
-        localStorage.setItem('MAX_QUESTIONS', MAX_QUESTIONS)
+    localStorage.setItem('mostRecentScore', score)
+    localStorage.setItem('questionCounter', questionCounter)
+    localStorage.setItem('MAX_QUESTIONS', MAX_QUESTIONS)
+    localStorage.setItem('finalScore', finalScore)
 
     questionCounter++
 
-
+    let progressCount = questionCounter - 1
+    
     questionHtmlCounter.innerText = ` ${questionCounter} of ${MAX_QUESTIONS}`
 
-    progressBarFull.style.width = `${(questionCounter / MAX_QUESTIONS ) * 100}%`
-    
+    progressBarFull.style.width = `${(progressCount / MAX_QUESTIONS) * 100}%`
+
     const questionIndex = Math.floor(Math.random() * availableQuestions.length);
     currentQuestion = availableQuestions[questionIndex]
     questions.innerText = currentQuestion.question
@@ -136,6 +140,8 @@ choices.forEach((choice) => {
         const classToApply = selectedAnswer == currentQuestion.answer ? 'correct' : 'incorrect'
         if (classToApply === 'correct') {
             incrementScore(CORRECT_BONUS)
+            incrementFinal(1)
+            
         }
 
         selectedChoice.parentElement.classList.add(classToApply)
@@ -153,6 +159,12 @@ startGame()
 const incrementScore = num => {
     score += num
     scoreCounter.innerText = `Score: ${score}`
+
+}
+
+const incrementFinal = num => {
+    finalScore += num
+    console.log(finalScore)
 
 }
 
